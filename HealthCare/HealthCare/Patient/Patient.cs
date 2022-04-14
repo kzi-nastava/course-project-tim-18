10807db2473c94fa5ce89b666ad046a18c3657f7
Marko Pejanovic <markop39@gmail.com>
2022-04-14 18:26:13 +0200

    Patient begin

---
+++ HealthCare/HealthCare/Patient/Patient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Patient
+{
+    public class Patient : User
+    {
+
+    }
+}
