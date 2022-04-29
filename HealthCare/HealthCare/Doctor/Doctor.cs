d70e2f478109c7027f839ffc9fa4bc9f66882c33
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2022-04-29 06:25:22 +0200

    Create appointment function finished

--- HealthCare/HealthCare/Doctor/Doctor.cs
+++ HealthCare/HealthCare/Doctor/Doctor.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HealthCare.Patient;
+﻿
+using System.ComponentModel.DataAnnotations;
 
 namespace HealthCare.Doctor
 {
@@ -12,16 +8,20 @@ namespace HealthCare.Doctor
         public string Name;
         public string Surname;
         private string id;
-        private List<Appointment> appointments;
+        private List<Appointment>? appointments;
 
         public Doctor() {
             username = "";
             password = "";
-            appointments = null;
+            appointments = new List<Appointment>();
             id = "";
             Name = "";
             Surname  = "";
         }
+
+        public string Id { get; set; }
+
+        public List<Appointment> Appointments { get; set; }
 
         public Doctor(string username, string password,string name, string surname, string id, List<Appointment> appointments)
         {
@@ -32,5 +32,36 @@ namespace HealthCare.Doctor
             this.id = id;
             this.appointments = appointments;
         }
+        bool CreateAppointment()
+        {
+            Console.Write("Unesite  ime i prezime pacijenta:  ");
+            string patientFullName = Console.ReadLine();
+            if (patientFullName == "") // TODO or check if username exists
+            {
+                Console.WriteLine("Neodgovarajuc unos");
+                return false;
+            }
+            Console.Write("Unesite datum i vreme pregleda pregleda(format = DD/MM/YYYY hh:mm):  ");
+            string dateTime = Console.ReadLine();
+            if (dateTime == "" ) // TODO regex for datetime
+            {
+                Console.WriteLine("Neodgovarajuc unos");
+                return false;
+            }
+            Console.WriteLine("Izaberite tip termina:");
+            Console.WriteLine("1. Operacija");
+            Console.WriteLine("2. Pregled");
+            string type = Console.ReadLine();
+            if (type != "1" || type != "2")
+            {
+                Console.WriteLine("Neodgovarajuc unos");
+                return false;
+            }
+            int z = Int32.Parse(type);
+            // TODO patient = new Patient(); 
+            this.appointments.Add(new Appointment(this, patientFullName, DateTime.ParseExact(dateTime, "dd/MM/yyyy HH:mm", null), (AppointmentType)Int32.Parse(type)-1));
+            return true;
+        }
     }
+    
 }
