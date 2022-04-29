fcf0631778752f6f8e7ae4151a1a48c8e49aa250
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2022-04-29 06:24:41 +0200

    Update Appointment class

--- HealthCare/HealthCare/Doctor/Appointment.cs
+++ HealthCare/HealthCare/Doctor/Appointment.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using System.Text.Json.Serialization;
 
 namespace HealthCare.Doctor
 {
     class Appointment
     {
+        private Doctor? doctor;
+        private string patient;
+        private DateTime? dateTime;
+        private AppointmentType? type;
+
+        public Appointment()
+        {
+            doctor = null;
+            patient = null;
+            dateTime = null;
+            type = null;
+        }
+
+        [JsonConstructor]
+        public Appointment(Doctor doctor, string patient, DateTime dateTime, AppointmentType type)
+        {
+            this.doctor = doctor;
+            this.patient = patient;
+            this.dateTime = dateTime;
+            this.type = type;
+        }
     }
 }
