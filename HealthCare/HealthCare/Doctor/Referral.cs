00cd3e446da9af88f5082b8936e5f27714474007
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2022-05-11 04:38:56 +0200

    Add referral class

---
+++ HealthCare/HealthCare/Doctor/Referral.cs
@@ -0,0 +1,25 @@
+﻿namespace HealthCare.Doctor;
+
+public class Referral
+{
+    private string doctor;
+    private string pacient;
+
+    public string Doctor {
+        get => doctor;
+        set => doctor = value;
+    }
+
+    public string Pacient
+    {
+        get => pacient;
+        set => pacient = value;
+    }
+
+    public Referral(string doctor, string pacient)
+    {
+        this.doctor = doctor;
+        this.pacient = pacient;
+    }
+
+}
